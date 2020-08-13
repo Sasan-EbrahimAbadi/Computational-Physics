cbe3c0f7b46a5d7981788a6394b89eff74c813eb
Sasan-EbrahimAbadi <51269101+Sasan-EbrahimAbadi@users.noreply.github.com>
2020-08-14 00:54:12 +0430

    Update Q1-3.m

--- Set2/Q1-3.m
+++ Set2/Q1-3.m
@@ -14,24 +14,24 @@ digits(16);
 Data=load('data_error.txt');
 N_F=100; % number of files
 for i=1:N_F;
-v=genvarname(['file' num2str(i)]); %genvarname constructs a variable name A with number squence of i
+    v=genvarname(['file' num2str(i)]); %genvarname constructs a variable name A with number squence of i
     m=(floor(size(Data,1)/N_F))*(i-1);
     n=(floor(size(Data,1)/N_F))*i;
     eval([v ' =Data(m+1:n,:)';]); %for deviding datas' from 1:328/329:328*2.....
     X=eval(['file' num2str(i)]); 
-   writetable(table(X),['file' num2str(i) '.txt'],'Delimiter','\t'); 
+    writetable(table(X),['file' num2str(i) '.txt'],'Delimiter','\t'); 
 end
 %computing varieance  mean value and standard deviation for each data set
 ans=zeros(N_F,3);
 for i=1:N_F;
- X=eval(['file' num2str(i)]);
- sigma2=X(:,3).^2;
- variance=1/sum((1./sigma2));
- Mean=sum((X(:,2)./sigma2))*variance;
- standarddeviation=sqrt(variance);
- ans(i,1)=Mean;
- ans(i,2)=standarddeviation;
- ans(i,3)=variance;
+    X=eval(['file' num2str(i)]);
+    sigma2=X(:,3).^2;
+    variance=1/sum((1./sigma2));
+    Mean=sum((X(:,2)./sigma2))*variance;
+    standarddeviation=sqrt(variance);
+    ans(i,1)=Mean;
+    ans(i,2)=standarddeviation;
+    ans(i,3)=variance;
 end
 for i=1:N_F;
     m=size(X,1)*(i-1);
