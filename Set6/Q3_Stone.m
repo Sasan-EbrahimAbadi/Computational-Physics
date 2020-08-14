38f8bf023ccf134141308c7056f6001eda920fb2
Sasan-EbrahimAbadi <51269101+Sasan-EbrahimAbadi@users.noreply.github.com>
2020-08-14 15:48:01 +0430

    Update Q3_Stone.m

--- Set6/Q3_Stone.m
+++ Set6/Q3_Stone.m
@@ -1,22 +1,22 @@
 clear
 M=100;
 for N=50000:10000:100000;
-m=0;
-for i=1:M
-    m=m+1;
-    Ns=0;
-    for j=1:N
-        x=rand;
-        y=rand;
-        f=sqrt(1-(x^2));
-        if y<f
-            Ns=Ns+1;
+    m=0;
+    for i=1:M
+        m=m+1;
+        Ns=0;
+        for j=1:N
+            x=rand;
+            y=rand;
+            f=sqrt(1-(x^2));
+            if y<f
+                Ns=Ns+1;
+            end
         end
+        A(m,1)=4*(Ns/N);
     end
-    A(m,1)=4*(Ns/N);
-end
-As=(sum(A)/M);
-save(['P' num2str(N) '.mat' ],'As')
+    As=(sum(A)/M);
+    save(['P' num2str(N) '.mat' ],'As')
 end
 for i=50000:10000:100000
     q=load(['P' num2str(i) '.mat']);
